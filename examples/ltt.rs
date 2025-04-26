86e133d8f8508d4a2becbd1e111ed49addfcccf1
Karolis Ramanauskas <karolisr@gmail.com>
2025-04-25 21:36:08 -0500

    - Fixes LTT function. - Adds a few big trees for testing.

--- examples/ltt.rs
+++ examples/ltt.rs
@@ -2,23 +2,27 @@ use dendros::Tree;
 use dendros::flatten_tree;
 use dendros::ltt;
 use dendros::parse_newick;
+use std::fs::read_to_string;
 
 fn main() {
-    let data = "(IX:0.9,(VII:0.7,(V:0.5,IV:0.4,(II:0.2,I:0.1)III:0.3)VI:0.6)VIII:0.8)R;";
-    let data = String::from(data);
+    // let data = "(IX:0.9,(VII:0.7,(V:0.5,IV:0.4,(II:0.2,I:0.1)III:0.3)VI:0.6)VIII:0.8)R;";
+    // let data = String::from(data);
+    // let data = read_to_string("tests/data/tree01.newick").unwrap();
+    let data = read_to_string("tests/data/big_seed_plant_trees/ALLOTB.tre").unwrap();
 
-    let tree = match parse_newick(data) {
+    let mut tree = match parse_newick(data) {
         Some(t) => t,
         None => Tree::new(),
     };
 
-    println!("\n{}", &tree);
+    tree.unroot();
+
+    // println!("\n{}", &tree);
 
     let edges = flatten_tree(&tree);
-    let epsilon = tree.height() / 1e2;
-    let points = ltt(&edges, 10, epsilon);
+    let points = ltt(&edges, 100);
 
     for pt in points {
-        println!("{:>7.2}: {:>5}", pt.time, pt.count);
+        println!("{:>7.2}: {:>10}", pt.time, pt.count);
     }
 }
--- src/phylo/ltt.rs
+++ src/phylo/ltt.rs
@@ -7,25 +7,23 @@ pub struct LttPoint {
     pub count: usize,
 }
 
-pub fn ltt(edges: &Edges, sample_count: usize, epsilon: TreeFloat) -> Vec<LttPoint> {
-    let sample_size = 1e0 / sample_count as TreeFloat;
-    let mut sample_counts: Vec<usize> = Vec::new();
+pub fn ltt(edges: &Edges, sample_count: usize) -> Vec<LttPoint> {
+    let sample_every = 1e0 / sample_count as TreeFloat;
+    let mut counts: Vec<usize> = Vec::new();
     let mut sample_points: Vec<TreeFloat> = Vec::new();
     for sample_idx in 1..=sample_count {
-        let sample_point = sample_idx as TreeFloat * sample_size;
-        let mut sample_count: usize = 0;
-        let mut sample_edges: Edges = Vec::new();
+        let sample_point = sample_idx as TreeFloat * sample_every;
+        let mut current_count: usize = 0;
         for e in edges {
-            if e.x0 < sample_point && e.x1 >= sample_point - epsilon {
-                sample_count += 1;
-                sample_edges.push(e.clone());
+            if e.x0 < sample_point && e.x1 >= sample_point - sample_every / 1e1 {
+                current_count += 1;
             }
         }
         sample_points.push(sample_point);
-        sample_counts.push(sample_count);
+        counts.push(current_count);
     }
 
-    let points: Vec<(TreeFloat, usize)> = zip(sample_points, sample_counts).collect();
+    let points: Vec<(TreeFloat, usize)> = zip(sample_points, counts).collect();
     let points: Vec<LttPoint> = points
         .iter()
         .map(|a| LttPoint { time: a.0, count: a.1 })
