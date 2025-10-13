f50be9131aa74e8568bf858e536b04aec3ccf126
Karolis Ramanauskas <karolisr@gmail.com>
2025-10-13 14:44:50 -0500

    Updates the build shell script.

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-# rustc --print=target-cpus
-# rustc --print cfg
-# rustc -Ctarget-cpu=generic --print cfg
-# rustc -Ctarget-cpu=native --print cfg
+# -------------------------------------------
+# Suppress printing of error messages
+# exec 2>/dev/null
 
-# RUSTFLAGS="-Ctarget-cpu=generic"
-# RUSTFLAGS="-Ctarget-cpu=native"
-# export RUSTFLAGS
+# Stop on first error
+set -o errexit
+# Set trap on ERR to be inherited by shell functions
+set -o errtrace
+
+# Trap errors
+trap 'echo Error at line: $LINENO' ERR
+# -------------------------------------------
 
 cargo fmt && \
-
-cargo test --tests && \
 
 cargo check --profile dev && \
 cargo clippy --profile dev && \
@@ -19,4 +21,9 @@ cargo build --profile dev && \
 
 cargo check --profile release && \
 cargo clippy --profile release && \
-cargo build --profile release
+cargo build --profile release && \
+
+cargo test --tests --profile dev && \
+cargo test --tests --profile release && \
+
+echo -e "OK"
